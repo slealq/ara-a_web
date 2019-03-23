2ec72caea4cb95d9172db98300c2f9000eb4321e
Stuart Leal <stuart.leal23@gmail.com>
2019-03-23 00:53:00 -0600

    Code refactor, add test scripts and setup
    This code should become a python module in
    a near future. This change is a first step
    towards that direction.

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/slealq/tcu677_webcrawler
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='tcu677-webcrawler',
+    version='0.1.0',
+    description='Simple web crawler to fetch text from news\' webpages. ',
+    long_description=readme,
+    author='Stuart Leal | Josue Rojas | Sergio Lizano',
+    author_email='stuart.leal23@gmail.com',
+    url='https://github.com/slealq/tcu677_webcrawler',
+    license=license,
+    packages=find_packages(exclude=('tests'))
+)
---
+++ tests/test_get_news_urls.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from 
+
+def 
+
+if __name__ == "__main__":
