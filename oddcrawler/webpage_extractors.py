68d3d78863e2253fd468ab2b0fdeeb3f58e390dc
Stuart Leal Q <stuart.leal23@gmail.com>
2019-04-13 18:19:08 -0600

    Fix get urls and add extract text func
    In this commit, the code that extracts all news urls from
    a given date if fixed. Also the tests now includes a
    correction too.
    
    Also, the function that extracts text from all the urls,
    is implemented and tested.

--- oddcrawler/webpage_extractors.py
+++ oddcrawler/webpage_extractors.py
@@ -3,6 +3,8 @@
 
 from abc import ABC, abstractmethod
 from logging import getLogger
+from datetime import timedelta, date
+from json import dumps
 from selenium.webdriver import Firefox
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -35,51 +37,160 @@ class webpage_extractor(ABC):
 
 
 class monumental_extractor(webpage_extractor):
+
+    LOAD_TIMEOUT = 10
+    MAX_DAYS_BEFORE = 7
+    PAGE_NOT_FOUND = 'Page not found - Monumental'
+
     def __init__(self):
         self._main_url = "http://www.monumental.co.cr/{year}/{month}/{day}"
+        self._subpage_url = "{main_url}/page/{page_number}"
         # If logger isn't configured outside this module, there won't be logs
         self._logger = getLogger(
             'oddcrawler.webpage_extractors.monumental_extractor')
         self._urls_xpath = '/html/body/div[3]/div[4]/section/div/div/section'
-        self._urls_class = 'col-md-7 col-xs-12 content-noticias archive'
+        self._articles_class = \
+            'col-md-12 no-pad noticia noticia-vertical nota-interna listado'
         self._driver = Firefox()
+        self._datetime_date = date.today()
 
-    def wait_until_page_loads(self):
-        TIMEOUT = 10
-        try:
-            WebDriverWait(self._driver, TIMEOUT).until(
-                EC.presence_of_element_located((By.XPATH, self._urls_xpath)))
-        except TimeoutException:
-            raise WebDriverException("Page didn't load correctly")
+    def __wait_until_page_loads(self):
+        """Wait unitl LOAD_TIMEOUT seconds has passed, to confirm there's
+        the xpath of the section that contains the news. If it isn't present,
+        raise a TimeuutException."""
+        WebDriverWait(self._driver, self.LOAD_TIMEOUT).until(
+            EC.presence_of_element_located((By.XPATH, self._urls_xpath)))
 
-    def get_news_urls(self, datetime_date):
-        year = datetime_date.year
-        month = datetime_date.month
-        day = datetime_date.day
+        self._logger.info('Page loaded succesfully')
 
-        self._logger.info(
-            'Fetch urls from date {day}/{month}/{year}'.format(**locals()))
-        entry_url = self._main_url.format(**locals())
+    def __get_day_month_year_from_datetime(self, datetime_date):
+        self._datetime_date = datetime_date
+        self._year = datetime_date.year
+        self._month = datetime_date.month
+        self._day = datetime_date.day
+
+    def __fetch_urls(self):
+        """Format the main url with day, month and year, and command a driver
+        to hit the formed url."""
+        self._logger.info('Fetch urls from date {day}/{month}/{year}'.format(
+            day=self._day,
+            month=self._month,
+            year=self._year))
+
+        self._entry_url = self._main_url.format(day=self._day,
+                                                month=self._month,
+                                                year=self._year)
 
         self._logger.info('Entry point: {entry_url}'.format(
-            entry_url=entry_url))
+            entry_url=self._entry_url))
 
         self._logger.info('Starting a new driver using Firefox')
-        self._driver.get(entry_url)
+        self._driver.get(self._entry_url)
 
-        self._logger.info('Wait until articles are formed, by xpath')
-        self.wait_until_page_loads()
+    def get_news_urls(self, datetime_date):
+        """Try to get the urls from news of the start date. If page fails
+        to load, then try with the previous day. Until a maximun of 7 days
+        before the given datetime_date."""
 
-        self._logger.info('Get urls using class')
-        urls = self._driver.find_elements_by_class_name(self._urls_class)
+        self.__get_day_month_year_from_datetime(datetime_date)
 
-        return [urls]
+        loaded = False
+        counter = self.MAX_DAYS_BEFORE
+        self.news_urls = []
+
+        # Get the date of the news right. Try to use the given one.
+        # If it doesn't work, use the previous one
+        while not loaded and counter > 0:
+            counter -= 1
+            try:
+                self.__fetch_urls()
+
+                self._logger.info('Wait until articles are formed, by xpath')
+                self.__wait_until_page_loads()
+
+                loaded = True
+            except TimeoutException:
+                # Try no longer than one week ago
+                self.__get_day_month_year_from_datetime(
+                    self._datetime_date - timedelta(days=1))
+
+                # Assert the reason is the page doesn't exist
+                assert self._driver.title == self.PAGE_NOT_FOUND
+
+                self._logger.info(
+                    'Page not found. Checking one day before.')
+
+        # Now iterate through all pages, and return a list with all urls
+        page = 1
+
+        while self._driver.title != self.PAGE_NOT_FOUND:
+            self._logger.info('Hitting page {number}'.format(number=page))
+
+            self._logger.info('Get articles by tag name')
+            articles = self._driver.find_elements_by_tag_name('article')
+
+            for each_article in articles:
+                assert each_article.get_attribute(
+                    'class') == self._articles_class
+
+                self.news_urls.append(
+                    each_article.find_element_by_tag_name(
+                        'a').get_attribute('href'))
+
+            page += 1
+
+            self._driver.get(self._subpage_url.format(
+                main_url=self._entry_url,
+                page_number=page))
+
+        return self.news_urls
 
     def filter_news_by_keywords():
         pass
 
-    def extract_text_from_news():
-        pass
+    def extract_text_from_news(self):
+        """Given that the news urls has been resolved, extract the text from
+        all news urls. Create a dictionary, which contains the url of the
+        news as keys, and the text as values."""
+
+        result = {}
+
+        for each_new_url in self.news_urls:
+            self._logger.info(
+                'Extract data from {url}'.format(url=each_new_url))
+
+            self._driver.get(each_new_url)
+
+            all_paragraphs = self._driver.find_elements_by_tag_name('p')
+
+            for p in all_paragraphs:
+                print(p.get_attribute('style'))
+                if p.get_attribute('style') == 'text-align: justify;':
+                    print('match')
+                    print(p.text)
+                else:
+                    print('nomatch')
+
+            valid_paragraphs = [p.text if p.get_attribute('style') ==
+                                'text-align: justify;' else '' for p in
+                                self._driver.find_elements_by_tag_name('p')]
+
+            result[each_new_url] = ' '.join(valid_paragraphs)
+
+        # Just in case we are running with set on force urls
+        # Define day, month and year, with the date defined at build time
+        # in __init__.
+        self.__get_day_month_year_from_datetime(self._datetime_date)
+
+        # Write result to a file, with current date.
+        with open('complete_news_from_{day}_{month}_{year}'.format(
+                day=self._day,
+                month=self._month,
+                year=self._year), 'w') as f:
+            f.write(dumps(result))
+            f.close()
+
+        return result
 
     def __del__(self):
         self._driver.quit()
--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -10,6 +10,27 @@ def test_get_news_urls(webpage_extractor):
     urls = webpage_extractor.get_news_urls(date.today())
     assert isinstance(urls, list)
 
+    return urls
+
+def test_extract_text_from_news(webpage_extractor):
+    """Needs `get_news_urls` to have been run first. Otherwise, assertion
+    of news_urls will fails.
+
+    It's expected that the result is a dictionary, and it contains urls
+    as keys and text of the news as values.
+    """
+
+    webpage_extractor.news_urls = ['http://www.monumental.co.cr/2019/04/12/contraloria-duda-de-solvencia-financiera-de-esph-y-senala-debilidades-de-control-interno/', 'http://www.monumental.co.cr/2019/04/12/recope-solicita-aumento-de-%c2%a273-por-litro-en-gasolina-super-y-%c2%a267-en-la-regular/', 'http://www.monumental.co.cr/2019/04/12/autoridades-activan-operativo-para-disminuir-accidentes-y-muertes-violentas-en-semana-santa/', 'http://www.monumental.co.cr/2019/04/12/operativo-contra-piques-en-pavas-dejo-vehiculos-alcohol-y-droga-decomisados/', 'http://www.monumental.co.cr/2019/04/12/pais-declara-estado-de-emergencia-por-listas-de-espera-de-donacion-y-trasplante-de-organos/', 'http://www.monumental.co.cr/2019/04/12/exdiputada-alexandra-loria-presenta-recurso-de-amparo-contra-camiseta-de-la-ucr-favor-del-aborto/', 'http://www.monumental.co.cr/2019/04/12/ctp-autorizo-25-empresas-de-buses-para-aumentar-flotilla-durante-semana-santa/', 'http://www.monumental.co.cr/2019/04/12/el-detalle-del-bano-de-mujeres-del-estadio-del-liverpool-que-esta-recorriendo-el-mundo/', 'http://www.monumental.co.cr/2019/04/12/zidane-contaria-con-keylor-navas-para-la-proxima-temporada/', 'http://www.monumental.co.cr/2019/04/12/los-mensajes-del-ex-general-chavista-hugo-carvajal-que-buscaban-un-acuerdo-con-la-justicia-de-eeuu-antes-de-ser-arrestado/', 'http://www.monumental.co.cr/2019/04/12/fuerte-sequia-por-el-fenomeno-del-nino-ocasiona-que-el-pais-cuente-con-un-55-menos-de-agua/', 'http://www.monumental.co.cr/2019/04/12/decomisan-armas-en-casa-de-extranjero-que-hirio-de-bala-a-dos-policias-en-bagaces/', 'http://www.monumental.co.cr/2019/04/12/visitara-la-playa-en-semana-santa-tenga-precaucion-con-las-fuertes-mareas-que-se-presentan-en-esta-epoca/', 'http://www.monumental.co.cr/2019/04/12/conapam-pide-incluir-adultos-mayores-en-actividades-familiares-durante-semana-santa/', 'http://www.monumental.co.cr/2019/04/12/exsacerdote-mauricio-viquez-entre-los-mas-buscados-de-la-interpol/', 'http://www.monumental.co.cr/2019/04/12/masiva-visitacion-a-zonas-protegidas-atenta-contra-fauna-silvestre-durante-semana-santa/', 'http://www.monumental.co.cr/2019/04/12/fuerte-respaldo-encuesta-revela-que-solo-el-4-de-los-ticos-se-opone-las-vacunas/', 'http://www.monumental.co.cr/2019/04/12/avanza-en-el-congreso-plan-que-prohibe-casas-de-empeno-abrir-en-horario-nocturno/', 'http://www.monumental.co.cr/2019/04/12/banco-de-sangre-requiere-120-donadores-por-dia-para-atender-demanda-de-semana-santa/', 'http://www.monumental.co.cr/2019/04/12/diputada-cuestiona-publicidad-de-evento-que-promueve-bodas-entre-parejas-del-mismo-sexo/', 'http://www.monumental.co.cr/2019/04/12/usuarios-podran-hacer-propuestas-para-agilizar-los-servicios-de-la-ccss/', 'http://www.monumental.co.cr/2019/04/12/jair-bolsonaro-anuncio-un-aguinaldo-para-los-beneficiarios-de-subsidios-estatales-en-brasil/', 'http://www.monumental.co.cr/2019/04/12/tragedia-en-rio-de-janeiro-al-menos-dos-personas-murieron-por-el-derrumbe-de-dos-edificios/']
+
+    assert isinstance(webpage_extractor.news_urls, list) \
+        and webpage_extractor.news_urls
+
+    text = webpage_extractor.extract_text_from_news()
+
+    assert isinstance(text, dict)
+
+    return text
+
 
 if __name__ == "__main__":
     # Configure logger: oddcrawler needsd to be the top logger
@@ -31,8 +52,10 @@ if __name__ == "__main__":
     # Begin first test
     logger.info('Begin Monumental get_news_urls test')
     monumental_extractor = monumental_extractor()
-    urls = test_get_news_urls(monumental_extractor)
-    print(urls)
-    assert isinstance(urls, list)
+    # urls = test_get_news_urls(monumental_extractor)
+    # print(urls)
+
+    # Test the text extraction
+    test_extract_text_from_news(monumental_extractor)
 
     # Test for other extractors might go here
